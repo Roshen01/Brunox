19c9887e7b9ad0dc1b3de60f0f42c946c983fae0
Roshen01 <malsharoshen1@gmail.com>
2025-04-01 17:48:19 +0200

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autopayTimeout: 3000,
+        autoplayHoverPause: true,
+        items: 1,
+        animateIn: "animate__fadeInUp",
+    });
+  });
